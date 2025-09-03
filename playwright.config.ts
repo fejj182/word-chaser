c96e6c433982f5884bb7bfeee4e14f3343c2c7c3
Jeff Zinger <jeff.zinger@easyjet.com>
2025-09-03 15:59:52 +0200

    Always retry failing e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
   testDir: './tests/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'retain-on-failure',
